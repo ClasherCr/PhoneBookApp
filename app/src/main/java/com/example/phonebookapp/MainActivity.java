120e1e788fb57d3efa6ba5688fcb605cc6d1fedc
Jashkumar Solanki <jashkumarsolanki@gmail.com>
2024-06-17 16:10:13 +0530

    Final Build

--- app/src/main/java/com/example/phonebookapp/MainActivity.java
+++ app/src/main/java/com/example/phonebookapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +14,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.phonebookapp.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -23,21 +29,52 @@ public class MainActivity extends AppCompatActivity {
     private MyAdapter userAdapter;
     private ActivityMainBinding binding;
 
+
+
     @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database;
+        DatabaseReference databaseReference;
+
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Users");
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        recyclerView = binding.recyclerView;
 
-        userAdapter = new MyAdapter(this, users);
+        recyclerView = binding.recyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        userAdapter.notifyDataSetChanged();
+        users = new ArrayList<>();
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    users.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        userAdapter = new MyAdapter(this, users);
+        recyclerView.setAdapter(userAdapter);
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
